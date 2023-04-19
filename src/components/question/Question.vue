c2633ce127b9ed2b423518f4d1d4cc698e146c51
jackyliu <tsunhousam91@gmail.com>
2023-04-19 17:45:54 +0800

    ui調整

--- src/components/question/Question.vue
+++ src/components/question/Question.vue
@@ -73,7 +73,7 @@ function goToNext() {
 
 <style lang="scss" scoped>
 .question-root {
-    width: 100%;
+    width: calc(100% - 20px);
     height: 100%;
     border: solid black 1px;
     padding: 10px 10px;
